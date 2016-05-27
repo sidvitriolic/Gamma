6beef2ec294c44f6470d2e6fcebbbad6f7b6ccc4
tonybeato <abeato8012@gmail.com>
2016-05-27 16:51:04 -0400

    Bio
    Description page

---
+++ bio.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Gamma Interface</title>
+        <link rel="stylesheet" href="assets/css/style_main.css">
+    </head>
+    <body>
+        <div class="content-wrap">
+            <h1>About Gamma interface</h1>
+            <p>This prototyping interface is used for playing around with front end development.</p>
+        </div>
+        
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/functions.js"></script>
+    </body>
+</html>
